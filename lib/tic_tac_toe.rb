700eba88246e00f7879cc01ac5bd4eec518a1e65
Dylan Williamson <dirtyassweed@icloud.com>
2020-03-25 18:49:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,70 +1,21 @@
-require 'pry'
-
 class TicTacToe
-  
-  def initialize
-    @board = [" "," "," "," "," "," "," "," "," "]
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
   end
-  
-  def WIN_COMBINATIONS[]
-    self = [
-      [0,1,2]
-      [3,4,5]
-      [6,7,8]
-      [0,4,8]
-      [2,4,6]
-      [0,3,6]
-      [1,4,7]
-      [2,5,8]
-      ]
+ 
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
   end
-  
-  def self.display_board
-    @board
+ 
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
   end
-  
-  def self.input_to_index[num]
-    num = index -= 1
-    result
+ 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
-  def self.move[]
-  end
-  
-  def self.position_taken?
-  end
-
-  def self.valid_move?
-  end
-  
-  def self.turn 
-  end
-  
-  def self.turn_count
-  end 
-  
-  def self.current_player
-  end 
-  
-  def self.won? 
-  end 
-  
-  def self.full?
-  end 
-  
-  def self.draw?
-  end 
-  
-  def self.over?
-  end 
-  
-  def self.winner
-  end 
-  
-  def self.play 
-  end 
-  
-
 end
-
-binding.pry
