fe69480a5efaae8050a1f53f2e3992b59442fc7e
Dylan Williamson <dirtyassweed@icloud.com>
2020-03-25 19:03:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,11 @@
 class TicTacToe
+  
+  WIN_COMBINATIONS = 
+    
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
   
-  WIN_COMBINATIONS = 
  
  
   def current_player
