f98817824eecf9dc317e123e96ef5a534e3617e6
Dylan Williamson <dirtyassweed@icloud.com>
2020-03-25 19:23:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,6 @@
 class TicTacToe
   
-  def initialize(board = nil)
-    @board = board || Array.new(9, " ")
-  end
-  
- WIN_COMBINATIONS = [
+   WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -14,6 +10,10 @@ class TicTacToe
     [0,4,8],
     [2,4,6]
     ]
+  
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
   
   def self.input_to_index(x="")
     
